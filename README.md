531d115b8396fa9f439ffc2a6cc83812c060ae1d
Francesco Frassinelli <francesco.frassinelli@nina.no>
2022-12-21 17:00:27 +0100

    Use Hydra/OmegaConf

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # Usage
 
+Set `URL` variable first.
+
 ```
 redis=$(docker run --rm -p 6379:6379 --name pygeoapi-nina-redis -d redis)
 pdm install --no-self
-export PYGEOAPI_CONFIG=config.yml
-export PYGEOAPI_OPENAPI=openapi.yml
-pdm run pygeoapi openapi generate config.yml -of $PYGEOAPI_OPENAPI
+export PYGEOAPI_CONFIG=generated-config.yaml
+export PYGEOAPI_OPENAPI=generated-openapi.yaml
+pdm run ./update_config.py ++server.url=$URL
+pdm run pygeoapi openapi generate $PYGEOAPI_CONFIG -of $PYGEOAPI_OPENAPI
 pdm run pygeoapi serve # CTRL+C
 docker stop $redis
 ```
--- update_config.py
+++ update_config.py
@@ -1,56 +1,58 @@
 #!/usr/bin/env python3
 
-
 import datetime
 import os
+import tempfile
 from urllib.parse import urljoin
 
+import hydra
 import requests
 import yaml
+from omegaconf import OmegaConf, open_dict
 
 from nvdb import URL
 
 
-def main(pygeoapi_config):
-    with open(pygeoapi_config) as config_file:
-        config = yaml.safe_load(config_file)
+@hydra.main(version_base=None, config_path="conf", config_name="config")
+def main(cfg):
+    pygeoapi_config = os.environ["PYGEOAPI_CONFIG"]
 
-    object_types = requests.get(urljoin(URL, "/vegobjekttyper")).json()
-    for object_type in object_types:
-        resource = {
-            "type": "collection",
-            "title": f"[NVDB] {object_type['navn']}",
-            "description": (
-                f"{object_type.get('beskrivelse')}\n"
-                f"VegObjekter ID: {object_type['id']}"
-            ).lstrip(),
-            "keywords": ["NVDB", "VegObjekter"],
-            "links": [],
-            "extents": {
-                "spatial": {
-                    "bbox": [-180, -90, 180, 90],
-                    "crs": "http://www.opengis.net/def/crs/OGC/1.3/CRS84",
+    OmegaConf.set_struct(cfg, True)
+    with open_dict(cfg):
+        object_types = requests.get(urljoin(URL, "/vegobjekttyper")).json()
+        for object_type in object_types:
+            resource = {
+                "type": "collection",
+                "title": f"[NVDB] {object_type['navn']}",
+                "description": (
+                    f"{object_type.get('beskrivelse')}\n"
+                    f"VegObjekter ID: {object_type['id']}"
+                ).lstrip(),
+                "keywords": ["NVDB", "VegObjekter"],
+                "links": [],
+                "extents": {
+                    "spatial": {
+                        "bbox": [-180, -90, 180, 90],
+                        "crs": "http://www.opengis.net/def/crs/OGC/1.3/CRS84",
+                    },
                 },
-            },
-            "providers": [
-                {
-                    "type": "feature",
-                    "name": "nvdb.vegobjekter.VegObjekter",
-                    "data": "/dev/null",
-                    "options": {"obj_id": object_type["id"]},
-                }
-            ],
-        }
-        temporal_end = object_type.get("objektliste_dato")
-        if temporal_end:
-            resource["temporal"] = {"end": datetime.date.fromisoformat(temporal_end)}
-        config["resources"][f"nvdb_{object_type['kortnavn']}"] = resource
+                "providers": [
+                    {
+                        "type": "feature",
+                        "name": "nvdb.vegobjekter.VegObjekter",
+                        "data": "/dev/null",
+                        "options": {"obj_id": object_type["id"]},
+                    }
+                ],
+            }
+            temporal_end = object_type.get("objektliste_dato")
+            if temporal_end:
+                resource["temporal"] = {"end": temporal_end}
+            cfg["resources"][f"nvdb/{object_type['kortnavn']}"] = resource
 
-    dumped_yaml = yaml.dump(config)
     with open(pygeoapi_config, "w") as config_file:
-        config_file.write(dumped_yaml)
+        config_file.write(OmegaConf.to_yaml(cfg))
 
 
 if __name__ == "__main__":
-    pygeoapi_config = os.environ["PYGEOAPI_CONFIG"]
-    main(pygeoapi_config)
+    main()
